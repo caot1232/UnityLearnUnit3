f61218bbc9eaf9f43197f67255ba7aeaa1fc6429
theresa <caot@roverkids.org>
2026-01-09 14:01:51 -0500

    complete 3.4

--- JumpForce/JumpForce/Assets/scripts/PlayerController.cs
+++ JumpForce/JumpForce/Assets/scripts/PlayerController.cs
@@ -5,6 +5,11 @@ public class PlayerController : MonoBehaviour
    
     private Rigidbody playerRb;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -15,7 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-        
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
        
     }
@@ -28,6 +33,8 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -35,12 +42,16 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("obstacle"))
         {
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b" , true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
 
         
